7052677ad847f86e739a60af14504da14825c942
Rolit19 <rolit454@gmail.com>
2021-09-21 13:06:34 +0530

    update

---
+++ precache-manifest.74672ba02b50838a69ee70c2b3a3f071.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88001f46735c3eb9bae6c7110d7146ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f40532187d6a42bb997f",
+    "url": "/static/css/2.bf73ea94.chunk.css"
+  },
+  {
+    "revision": "2def70b14bc50ab7e9aa",
+    "url": "/static/css/main.c9926f46.chunk.css"
+  },
+  {
+    "revision": "f40532187d6a42bb997f",
+    "url": "/static/js/2.29eef005.chunk.js"
+  },
+  {
+    "revision": "cc9868e3a4f3086f732c533829a63bda",
+    "url": "/static/js/2.29eef005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d78132c054dfab4127",
+    "url": "/static/js/3.de99490a.chunk.js"
+  },
+  {
+    "revision": "2def70b14bc50ab7e9aa",
+    "url": "/static/js/main.183d4746.chunk.js"
+  },
+  {
+    "revision": "484b79fdead92e729433",
+    "url": "/static/js/runtime-main.1dd28327.js"
+  },
+  {
+    "revision": "c3b7cec5e7f2f6fa81d529e32a573178",
+    "url": "/static/media/ApexLogo.c3b7cec5.jpg"
+  },
+  {
+    "revision": "46ccf87797d7e8c06d8c4fe952eb7740",
+    "url": "/static/media/ab1.46ccf877.jpg"
+  },
+  {
+    "revision": "dd2e604ea615a234f42d104afa624057",
+    "url": "/static/media/ab2.dd2e604e.jpg"
+  },
+  {
+    "revision": "a84bad42d68583ce3e928ff8dadadf8a",
+    "url": "/static/media/ab4.a84bad42.png"
+  },
+  {
+    "revision": "11db147aad128a91b6c3112d85e76a6f",
+    "url": "/static/media/c1.11db147a.png"
+  },
+  {
+    "revision": "4a48a3453aeb37487fb49102ba639bfb",
+    "url": "/static/media/c2.4a48a345.jpg"
+  },
+  {
+    "revision": "87a7cd70caa5073c883f5e307275d8e1",
+    "url": "/static/media/c3.87a7cd70.jpg"
+  },
+  {
+    "revision": "08ea49df372f48e17b524e609a2b1db5",
+    "url": "/static/media/c4.08ea49df.jpg"
+  },
+  {
+    "revision": "316d0177e263bd97efe302cf0cd20688",
+    "url": "/static/media/c5.316d0177.jpg"
+  },
+  {
+    "revision": "ccea4c951963d16137e152b98d026364",
+    "url": "/static/media/c6.ccea4c95.jpg"
+  },
+  {
+    "revision": "1f6a248d3849cdea517b982ea001c351",
+    "url": "/static/media/c7.1f6a248d.jpg"
+  },
+  {
+    "revision": "07dc8f994b923ac1a12477ed93e1f3c4",
+    "url": "/static/media/c8.07dc8f99.jpg"
+  },
+  {
+    "revision": "7f20768b43dc075f8f4aa3d6df823cc3",
+    "url": "/static/media/chem1.7f20768b.jfif"
+  },
+  {
+    "revision": "c80b547374cbcf6d68d74abb1ec9b518",
+    "url": "/static/media/chem2.c80b5473.jfif"
+  },
+  {
+    "revision": "805082bae65667c424eb8decf363570c",
+    "url": "/static/media/chem3.805082ba.jfif"
+  }
+]);
